b4afa26dcabed8e5a4d338446c08a688df64227f
eightnoteight <mr.eightnoteight@gmail.com>
2015-12-28 09:18:06 +0530

    little bit of details

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='tgbots',
+    version='0.1',
+    description='my cute little telegram bots',
+    author='Srinivas Devaki',
+    author_email='mr.eightnoteight@gmail.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+)
